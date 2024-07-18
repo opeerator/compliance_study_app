7c9047912e0620ec4fbb1508ed0dbf7422dc20ee
Ali Y <d.aliyamini@gmail.com>
2024-07-18 15:36:26 -0400

    update

--- app/App.js
+++ app/App.js
@@ -48,7 +48,7 @@ const App = () => {
     if (!isConnected) return; // prevent login if not connected
     try {
       const response = await axios.post(
-        'http://129.97.228.209:5000/login',
+        'http://129.97.228.209:8443/login',
         { hash_code: inputHashcode }
       );
       if (response.data.message === "Valid") {
